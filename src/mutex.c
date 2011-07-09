5bf5a4c4a12e92da357fdbfe64c42a7466fa1bdc
ktietz70 <ktietz70@4407c894-4637-0410-b4f5-ada5f102cad1>
2011-07-09 10:20:43 +0000

    Fix typo
    git-svn-id: https://mingw-w64.svn.sourceforge.net/svnroot/mingw-w64/experimental/winpthreads@4264 4407c894-4637-0410-b4f5-ada5f102cad1

--- src/mutex.c
+++ src/mutex.c
@@ -35,7 +35,7 @@ mutex_ref(pthread_mutex_t *m)
     int r = 0;
 
     _spin_lite_lock(&mutex_global);
-    if (!m || IsBasReadPtr (m, sizeof (void *))
+    if (!m || IsBadReadPtr (m, sizeof (void *))
         || !*m)
     {
       _spin_lite_unlock(&mutex_global);
@@ -97,7 +97,7 @@ mutex_ref_destroy(pthread_mutex_t *m, pthread_mutex_t *mDestroy )
     if (_spin_lite_trylock(&mutex_global))
       return EBUSY;
     
-    if (!m || IsBasReadPtr (m, sizeof (void *))
+    if (!m || IsBadReadPtr (m, sizeof (void *))
         || !*m) r = EINVAL;
     else {
         mutex_t *m_ = (mutex_t *)*m;
