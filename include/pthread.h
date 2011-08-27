bb41c2c4b6804c1f6bf32c42540f1cc4e6698f98
ktietz70 <ktietz70@4407c894-4637-0410-b4f5-ada5f102cad1>
2011-08-27 17:04:29 +0000

    Replace _ReadWriteBarrier by __sync_synchronize in pthread.h header.
    git-svn-id: https://mingw-w64.svn.sourceforge.net/svnroot/mingw-w64/experimental/winpthreads@4423 4407c894-4637-0410-b4f5-ada5f102cad1

--- include/pthread.h
+++ include/pthread.h
@@ -193,9 +193,9 @@ struct _pthread_cleanup
 #define pthread_cleanup_push(F, A)\
 {\
     const _pthread_cleanup _pthread_cup = {(F), (A), *pthread_getclean()};\
-    _ReadWriteBarrier();\
+    __sync_synchronize();\
     *pthread_getclean() = (_pthread_cleanup *) &_pthread_cup;\
-    _ReadWriteBarrier()
+    __sync_synchronize()
 
 /* Note that if async cancelling is used, then there is a race here */
 #define pthread_cleanup_pop(E)\
